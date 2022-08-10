1f12bdefbaf4c56e667dfa2fe5767c2ae47f3889
devonxfire <devonmartindale@gmail.com>
2022-08-10 15:44:33 +0200

    adding all files

---
+++ doggos.js
@@ -0,0 +1,48 @@
+
+
+const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
+const select = document.querySelector('.breeds');
+
+fetch(BREEDS_URL).then(function(response) {
+    return response.json();
+}).then(function(data) {
+    const breedsObject = data.message;
+    const breedsArray = Object.keys(breedsObject);
+    
+    
+    for (let i = 0; i < breedsArray.length; i++) {
+        const option = document.createElement('option');
+        option.value = breedsArray[i];
+        option.innerText = breedsArray[i];
+        select.appendChild(option);
+    }
+
+});
+    select.addEventListener('change', function(event) {
+        
+        const img = document.querySelector('.dog-img');
+   let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+  
+
+   getNewDoggo(url);
+    
+    
+   
+})
+
+const img = document.querySelector('.dog-img');
+const spinner = document.querySelector('.spinner');
+
+
+function getNewDoggo(url) {
+    spinner.classList.add('show');
+    img.classList.remove('show');
+    fetch(url).then(function(response) {
+        return response.json();})
+        .then(function(data) {
+                img.src = data.message;
+                spinner.classList.remove('show');
+                img.classList.add('show');
+
+        });
+    }
